eeea20196f28cca92ba7c1aa741061b7f848a7e1
Persia Dixon <contact@piecedigital.net>
2025-04-08 23:51:25 -0400

    RED DOT RED DOT

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-
 function App() {
+  console.log("App started");
+
   class Pixel {
     /** @type {number} */
     #x;
@@ -10,66 +10,105 @@ function App() {
     #color;
 
     constructor(x, y, color) {
-      this.SetPixel(x, y, color);
-      console.log("App started");
+      this.Pos = [x, y];
+      this.Col = color;
     }
 
-    SetColor(color) {
+    /**
+     * Returns colors
+     */
+    get Col() {
+      return this.#color;
+    }
+    /**
+     * Sets color
+     *
+     * @param {number} coords
+     */
+    set Col(color) {
       this.#color = color;
     }
 
-    SetPosition(x, y) {
+    /**
+     * Returns xy positions in an array
+     */
+    get Pos() {
+      return [this.#x, this.#y];
+    }
+    /**
+     * Sets xy positions from array
+     *
+     * @param {[number, number]} coords
+     */
+    set Pos([x, y]) {
       this.#x = x;
       this.#y = y;
     }
 
+    /**
+     * Defines pixel position and color
+     *
+     * @param {number} x
+     * @param {number} y
+     * @param {number} color
+     */
     SetPixel(x, y, color) {
-      this.SetPosition(x, y);
-      this.SetColor(color);
+      this.Pos = [x, y];
+      this.Col = color;
     }
   }
 
   class RenderScene {
     /** @type {[Pixel]} */
     #pixels = [];
-    /** @type {THREE.Scene} */
-    #scene;
-    /** @type {THREE.OrthographicCamera} */
-    #camera;
-    /** @type {THREE.WebGLRenderer} */
+    /** @type {CanvasRenderingContext2D} */
+    #ctx;
+    /** @type {HTMLCanvasElement} */
     #renderer;
+    /** @type {number} */
+    #width
+    /** @type {number} */
+    #height
 
     constructor() {
       if (typeof window != "undefined") {
-        let width      = 800;
-        let height     = 800;
-        this.#scene    = new THREE.Scene();
-        this.#camera   = new THREE.OrthographicCamera(
-          width / -2, width / 2,
-          height / -2, height / 2,
-          1, 1000);
-        this.#renderer = new THREE.WebGLRenderer();
+        this.#width      = 800;
+        this.#height     = 800;
 
-        renderer.setSize(width, height);
-        document.getElementById("render-scene")?.appendChild(renderer.domElement);
+        this.#renderer = document.createElement("canvas");
+        this.#renderer.width = this.#width;
+        this.#renderer.height = this.#height;
+
+        this.#ctx = this.#renderer.getContext("2d");
+
+        document.querySelector("#render-scene").appendChild(this.#renderer);
 
         console.log("Render scene initiated");
+        this.#ctx.fillStyle = "white";
+        this.#ctx.fillRect(0, 0, this.#width, this.#height);
+
+        // temporary test draw
+        this.DrawPixel(new Pixel(0, 0, "red"))
       }
     }
 
-    #DrawSquare(pixel) {
-      this.#renderer.domElement.getContext("2d");
-    }
-
     /**
+     * Draws a square based on the privided pixel parameter
+     *
      * @param {Pixel} pixel
      */
     DrawPixel(pixel) {
       if (pixel instanceof Pixel) {
-        this.#pixels.appendChild(pixel);
+        console.log("making pixel");
+        this.#pixels.push(pixel);
+        const [posX, posY] = pixel.Pos;
+        this.#ctx.fillStyle = pixel.Col;
+        this.#ctx.fillRect(posX, posY, 20, 20);
       }
     }
   }
+
+  new RenderScene();
 }
 
 App();
